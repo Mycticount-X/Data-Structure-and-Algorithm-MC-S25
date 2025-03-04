5263565539ffec012593e94a5fd8662d24e9850a
Mycticount Xeta Ahlovely <mxlovers77@gmail.com>
2025-03-04 11:47:39 +0700

    Project Baru: BJ Library

--- Project/BJLibrary.cpp
+++ Project/BJLibrary.cpp
@@ -107,11 +107,7 @@ bool Delete (char id[]) {
 	
 	// Cek prev
 	if (curr->prev != NULL) {
-		curr->prev->next = curr->next;
 	} else {
-		HashTable[key] = curr->next;
-	}
-	
 	// Cek next
 	if (curr->next != NULL) {
 		curr->next->prev = curr->prev;
