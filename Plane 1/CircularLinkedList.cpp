f4ab8585ea7d5b1f68e293a42a0241852e12d52f
Mycticount Xeta Ahlovely <mxlovers77@gmail.com>
2025-03-16 20:06:47 +0700

    Circular (Doubly) Linked List Siap

--- Plane 1/CircularLinkedList.cpp
+++ Plane 1/CircularLinkedList.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Data Structure with MX
 struct Zone {
 	int MTX;
 	Zone* next;
@@ -15,61 +16,207 @@ Zone* CreateZone (int MTX) {
 	newzone->MTX = MTX;
 	newzone->next = NULL;
 	newzone->prev = NULL;	
+	return newzone;
 }
 
-void PushHead (int MTX) {
+// Push Command
+void PushHead(int MTX) {
+    Zone* newzone = CreateZone(MTX);
+    
+    if (head == NULL) {
+        head = tail = newzone;
+        head->next = head;
+        head->prev = head;
+    } else {
+        tail->next = newzone;
+        newzone->prev = tail;
+        newzone->next = head;
+        head->prev = newzone;
+        head = newzone;
+    }
+}
+
+void PushSort (int MTX) {
 	Zone* newzone = CreateZone(MTX);
 	
 	if (head == NULL) {
 		head = tail = newzone;
-		head->next = head;
-		head->prev = head;
+        head->next = head;
+        head->prev = head;
+		return;
+		
+	} else if (MTX < head->MTX) {
+		tail->next = newzone;
+        newzone->prev = tail;
+        newzone->next = head;
+        head->prev = newzone;
+        head = newzone;
+        return;
+        
+	} else if (MTX > tail->MTX) {
+		tail->next = newzone;
+        newzone->prev = tail;
+        newzone->next = head;
+        head->prev = newzone;
+        tail = newzone;
+        return;
 	}
 	
-	else if (head == tail) {
-		head->next = newzone;
-		tail = newzone;
-		tail->next = head;
-		tail->prev = head;
+	Zone* curr = head->next;
+	while (curr != head && MTX > curr->MTX) {
+		curr = curr->next;
 	}
 	
-	else {
-		head->prev = newzone;
-		newzone->next = head;
-		head = newzone;
-		tail->next = head;
-		head->prev = tail;
+	curr->prev->next = newzone;
+	newzone->prev = curr->prev;
+	newzone->next = curr;
+	curr->prev = newzone;
+	
+}
+
+// Pop Command
+void PopHead() {
+    if (head == NULL) {
+        printf("Permukaan saat ini kosong!\n");
+        return;
+    }
+
+    Zone* del = head;
+
+    if (head == tail) {
+        head = tail = NULL;
+    } else {
+        head = head->next;
+        head->prev = tail;
+        tail->next = head;
+    }
+
+    free(del);
+    del = NULL;
+}
+
+void PopSearch (int MTX) {
+	if (head == NULL) {
+        printf("Permukaan saat ini kosong!\n");
+        return;
+    }
+    
+    Zone* del = head;
+    if (head->MTX == MTX) {
+    	if (head == tail) {
+    		free(del);
+	        head = tail = NULL;
+	    } else {
+	        head = head->next;
+	        head->prev = tail;
+	        tail->next = head;
+	        free(del);
+	    }
+	    del = NULL;
+	    printf("Zona berhasil dihapus!\n");
+        return;
+	}
+	
+    
+    del = head->next;
+    while (del != head && del->MTX != MTX) {
+        del = del->next;
+    }
+    
+    if (del->MTX == MTX) {
+    	if (del == tail) {
+            tail = tail->prev;
+        }
+        
+        del->next->prev = del->prev;
+        del->prev->next = del->next;
+
+        free(del);
+        printf("Zona berhasil dihapus!\n");
+	} else {
+		printf("Zona tidak diteukan!\n");
 	}
 }
 
-void PopHead () {
+// Alter Command
+void ViewPlane () {
 	if (head == NULL) {
 		printf("Permukaan saat ini kosong!\n");
 		return;
 	}
 	
-	else if (head == tail) {
-		free(head);
-		head = tail = NULL;
+	Zone* curr = head;
+	int cc = 1;
+    printf("==========================\n");
+    printf("      ZONA TERDAFTAR\n");
+    printf("==========================\n");
+    
+	do {
+		printf("Zone: %d\nMyctix Consentration: %d\n", cc, curr->MTX);
+		printf("--------------------------\n");
+		curr = curr->next;
+		++cc;
+	} while (curr != head);
+}
+
+void ClearPlane () {
+	if (head == NULL) {
+		printf("Permukaan saat ini kosong!\n");
 		return;
 	}
 	
-	Zone* del = head;
-	head = head->next;
+	Zone* curr = head;
+	Zone* next;
+	int cc = 0;
 	
-	// Sisa 2 Zone
-	if (head == tail) {
-		head->next = head;
-		head->prev = head;
-	} else {
-		head->prev = tail;
-		tail->next = head;
-	}
+	do {
+		next = curr->next;
+		free(curr);
+		curr = next;
+		++cc;
+	} while (curr != head);
 	
-	free(del);
-	del = NULL;
+	head = tail = NULL;
+	printf("Berhasil Menghapus %d Zona!\n", cc);
 }
 
 int main () {
+	// Test Case 1
+	PushHead(23);
+	PushHead(53);
+	PushHead(77);
+	ViewPlane();
+	ClearPlane();
+	ViewPlane();
+	
+	// Test Case 2
+	PushSort(50);
+	PushSort(20);
+	PushSort(80);
+	PushSort(40);
+	PushSort(60);
+	ViewPlane();
+	PopSearch(10);
+	PopSearch(40);
+	PopSearch(50);
+	PopSearch(20);
+	PopSearch(60);
+	PopSearch(80);
+	PopSearch(10);
+	ViewPlane();
+	
+	// Test Case 3
+	PushSort(50);
+	PushSort(20);
+	PushSort(80);
+	PushSort(40);
+	PushSort(60);
+	ViewPlane();
+	PopSearch(20);  // Hapus head
+	ViewPlane();
+	PopSearch(80);  // Hapus tail
+	ViewPlane();
+
+
 	return 0;
 }
