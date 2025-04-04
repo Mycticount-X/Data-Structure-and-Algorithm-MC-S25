8bd6636ed24a5e2f737b6103f7c857798946b507
Mycticount Xeta Ahlovely <mxlovers77@gmail.com>
2025-04-04 21:49:37 +0700

    Snippet BST

---
+++ Plane 5/Test BST.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct node {
+	int key;
+	struct node *left;
+	struct node *right;
+} *root;
+
+bool search(struct node *curr, int find){
+	//COMPLETE THE CODE!
+	node* temp = curr;
+	while (temp) {
+		if (find < temp->key) {
+			temp = temp->left;
+		} else if (find > temp->key) {
+			temp = temp->right;
+		} else {
+			return true;
+		}
+	}
+	return false;
+}
+
+int heightBinaryTree(struct node *node){
+	//COMPLETE THE CODE!
+	if (node == NULL) {
+		return 0;
+	}
+	
+	int left = heightBinaryTree(root->left);
+	int right = heightBinaryTree(root->right);
+	return 1 + (left > right ? left : right);
+}
+
+int main () {
+	return 0;
+}
