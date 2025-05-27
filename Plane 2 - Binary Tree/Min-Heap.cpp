b00f750580d21725c33ef3377727ce6b370e9534
Mycticount Xeta Ahlovely <mxlovers77@gmail.com>
2025-05-27 10:12:04 +0700

    feat: Min Heap

---
+++ Plane 2 - Binary Tree/Min-Heap.cpp
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 1000
+int Heap[MAX];
+int zone = 0;
+
+void Generate(int MTX) {
+    if (zone >= MAX) {
+        printf("Zone limit reached.\n");
+        return;
+    }
+
+    Heap[zone] = MTX;
+    int curr = zone;
+    zone++;
+
+    while (curr > 0 && Heap[curr] < Heap[(curr - 1) / 2]) {
+        int temp = Heap[curr];
+        Heap[curr] = Heap[(curr - 1) / 2];
+        Heap[(curr - 1) / 2] = temp;
+        curr = (curr - 1) / 2;
+    }
+}
+
+int ExtractMin() {
+    if (zone == 0) {
+        printf("Heap is empty.\n");
+        return -1;
+    }
+
+    int min = Heap[0];
+    Heap[0] = Heap[zone - 1];
+    zone--;
+
+    int curr = 0;
+    while (curr < zone) {
+        int left = 2 * curr + 1;
+        int right = 2 * curr + 2;
+        int smallest = curr;
+
+        if (left < zone && Heap[left] < Heap[smallest]) {
+            smallest = left;
+        }
+        if (right < zone && Heap[right] < Heap[smallest]) {
+            smallest = right;
+        }
+        if (smallest != curr) {
+            int temp = Heap[curr];
+            Heap[curr] = Heap[smallest];
+            Heap[smallest] = temp;
+            curr = smallest;
+        } else {
+            break;
+        }
+    }
+
+    return min;
+}
+
+int main () {
+    return 0;
+}
